f750c8d39fa17eada24b739b74fa22745b6697c3
kiku ishimoto <kikuish@gmail.com>
2020-06-18 20:39:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,5 +8,12 @@ class App < Sinatra::Base
   get '/' do
     erb :index
   end
+  post '/checkout' do 
+    
+    session[:item] = params[item]
+    @session = session 
+    erb :checkout
+    
+  end 
 
 end
